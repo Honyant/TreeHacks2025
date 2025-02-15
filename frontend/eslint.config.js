4a8aa37ef11028e1e0775d7fabce6c0fb3ca3f5b
Neil Song <song.neil.song@gmail.com>
2025-02-15 11:25:53 -0800

    add simple import sort plugin

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -21,6 +22,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -28,6 +30,33 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "import/order": "off",
+      "import/first": "off",
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            // Note that if you use the `node:` prefix for Node.js builtins,
+            // you can avoid this complexity: You can simply use "^node:".
+            ["^node:"],
+            // Packages. `react` and `@tanstack` related packages come first.
+            ["^react$", "^(react-dom|@tanstack)(/.*|$)", "^@?\\w"],
+            // Internal packages.
+            ["^(server)(/.*|$)"],
+            // Side effect imports.
+            ["^\\u0000"],
+            // @self imports.
+            ["^(@self)(/.*|$)\\w"],
+            // Parent imports. Put `..` last.
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Style imports.
+            ["^.+\\.s?css$"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "warn",
     },
-  }
+  },
 );
