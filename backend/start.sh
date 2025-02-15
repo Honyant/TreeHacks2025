c4f2ffda35b3b158957e32bcdfd2f4702e466440
Neil Song <song.neil.song@gmail.com>
2025-02-15 12:43:04 -0800

    prop changes

--- backend/start.sh
+++ backend/start.sh
@@ -4,8 +4,8 @@
 if [ -f pid.txt ]; then 
     UVICORN_PID=$(cat pid.txt)
     kill $(pgrep -P $UVICORN_PID)
-    rm pid.txt
-    sleep 1
+    rm pid.txt src/research.db
+    sleep 2
 fi
 nohup bash -c "source .venv/bin/activate && cd src && uvicorn main:app --reload" > output.log 2>&1 &
 echo $! > pid.txt
